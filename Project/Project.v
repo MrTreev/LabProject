fb94ffc7e5bb89a9e8cd385826a95db77f243b6e
Kaizhe Huang <hkz85825915@gmail.com>
2022-05-05 17:12:38 +1000

    Add TLE IO blueprint

--- Project/Project.v
+++ Project/Project.v
@@ -1,5 +1,19 @@
 module Project(
-	input ,
-	output
+	// To Avalon
+	input [18:0] Avalon_address,
+	input Avalon_read,
+	output [31:0] Avalon_readdata,
+	input Avalon_write,
+	input [31:0] Avalon_writedata,
+
+	// To ADV7513
+	output ADV_DE,
+	output ADV_Hsync,
+	output ADV_Vsync,
+	output ADV_CLK,
+	input ADV_HPD,
+	output [32:0] ADV_Data,
+	output ADV_I2C_SDA,
+	output ADV_I2C_SCL
 );
 endmodule
