999e7f0f447abc361b6e0b9317148923b64bd9ec
Kaizhe Huang <hkz85825915@gmail.com>
2022-05-09 22:00:24 +1000

    Add sample software

---
+++ Software/README.md
@@ -0,0 +1,5 @@
+## Build
+
+1. `mkdir build; cd build`
+2. `cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=TC-de10.cmake`
+3. `make`
---
+++ Software/src/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+
+#define FB_WIDTH 640
+#define FB_HEIGHT 480
+
+#define FPGA_ADDR_BASE 0x40000000 // TODO
+#define FB_ADDR_OFFSET 0x100 // TODO
+
+// TODO
+enum hdmi_i2c_reg {
+  HDMI_I2C_CR = 0x00,
+  HDMI_I2C_DR = 0x01,
+};
+
+int main(int argc, char** argv) {
+  (void) argc;
+  (void) argv;
+
+  char* video_file_name = NULL;
+
+  char optc;
+  while ((optc = getopt(argc, argv, "d:")) != -1) {
+    switch (optc) {
+      case 'd':
+        video_file_name = optarg;
+        break;
+      case '?':
+        exit(1);
+        break;
+      default:
+        ;
+    }
+  }
+
+  if (video_file_name == NULL) {
+    fprintf(stderr, "Video file name unspecified");
+    exit(1);
+  }
+
+  // Open video file
+  FILE* video = fopen(video_file_name, "r");
+  if (video == NULL) {
+    fprintf(stderr, "fopen(\"%s\"): %s\n", video_file_name, strerror(errno));
+    exit(1);
+  }
+
+  // Map memory from AXI
+  int mem = open("/dev/mem", O_WRONLY | O_SYNC);
+  if (mem == -1) {
+    fprintf(stderr, "open: %s\n", strerror(errno));
+    exit(-1);
+  }
+  uint8_t* fpga_shmem = mmap(NULL, FB_WIDTH * FB_HEIGHT, PROT_WRITE, MAP_SHARED, mem, FPGA_ADDR_BASE);
+  if (fpga_shmem == MAP_FAILED) {
+    fprintf(stderr, "mmap: %s\n", strerror(errno));
+    exit(-1);
+  }
+  close(mem);
+
+  // Setup HDMI via I2C
+  // TODO
+
+  // Front & back buffer
+  uint8_t* fb_front = fpga_shmem + FB_ADDR_OFFSET;
+  uint8_t* fb_back = malloc(FB_WIDTH * FB_HEIGHT);
+
+  // SIGALRM every 40ms (25Hz)
+  sigset_t sigset_alarm;
+  sigemptyset(&sigset_alarm);
+  sigaddset(&sigset_alarm, SIGALRM);
+  ualarm(40000, 40000);
+  
+  // Main loop
+  while (true) {
+    if (fread(fb_back, 1, FB_WIDTH * FB_HEIGHT, video) != FB_WIDTH * FB_HEIGHT) {
+      if (feof(video)) {
+        // Loop video
+        clearerr(video);
+        rewind(video);
+        continue;
+      } else {
+        fprintf(stderr, "File read error\n");
+        exit(-1);
+      }
+    }
+
+    // Wait for time for next frame
+    int sig_recvd = 0;
+    sigwait(&sigset_alarm, &sig_recvd);
+
+    memcpy(fb_front, fb_back, FB_WIDTH * FB_HEIGHT);
+  }
+}
