4b88cb1ac74a410f7c3b0b973474765abd0c6c43
MrTreev <mr.treev@gmail.com>
2022-05-12 12:20:28 +1000

    alterred some stuff, added verilog 2001 reference

--- Project/modules/Counter.v
+++ Project/modules/Counter.v
@@ -1,9 +1,11 @@
-module Counter #(MAX_COUNT)(clk,reset_n,value);
-	parameter MAX_COUNT
+module Counter
+	#(MAX_COUNT)
+	(clk,reset_n,value);
+	parameter MAX_COUNT;
 
-	input clk,
-	input reset_n,
-	output reg [$clog2(MAX_COUNT)-1:0] value = 0
+	input clk;
+	input reset_n;
+	output reg [$clog2(MAX_COUNT)-1:0] value = 0;
 
 
 always @(posedge(clk), negedge(reset_n))
--- Project/modules/NBitMWideRegisterFile.v
+++ Project/modules/NBitMWideRegisterFile.v
@@ -1,6 +1,8 @@
 // Lab Project
 // N-Bit M-Wide Register File
-module NBitMWideRegisterFile #(P_RegCount,P_BitWidth)(In_Address,In_WriteData,In_Write,In_Read,In_Reset_n,In_Clock_50MHz,Out_ReadData);
+module NBitMWideRegisterFile
+	#(P_RegCount,P_BitWidth)
+	(In_Address,In_WriteData,In_Write,In_Read,In_Reset_n,In_Clock,Out_ReadData);
 
 	parameter P_RegCount=3; 		// Number of registers
 	parameter P_BitWidth=32; 		// Bit Width for each register
@@ -10,7 +12,7 @@ module NBitMWideRegisterFile #(P_RegCount,P_BitWidth)(In_Address,In_WriteData,In
 	input In_Write;
 	input In_Read;
 	input In_Reset_n;
-	input In_Clock_50MHz;
+	input In_Clock;
 
 	output [P_BitWidth-1:0] Out_ReadData;
 
@@ -24,7 +26,7 @@ module NBitMWideRegisterFile #(P_RegCount,P_BitWidth)(In_Address,In_WriteData,In
 	generate
 		for (m=0; m<=(P_RegCount); m=m+1) begin: GenBlock
 			NBitRegister #(P_BitWidth,0) UCR_m(
-				In_Clock_50MHz, 	// Clock
+				In_Clock, 		// Clock
 				In_Reset_n, 		// Active low clear (async)
 				In_WriteData,           // Data input
 				R_Select[m],            // Active High Enable
